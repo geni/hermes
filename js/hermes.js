d0218d16376d49aa3423bedb08659e03b59aca63
Scott Steadman <scott.steadman@geni.com>
2019-11-26 13:39:33 -0800

    fix hermes.js
      I had to add the self mechanic back to the Hermes
      function.
    
      The self mechanic allows us to write the code cleaner
    
      For Example:
    
        var myHermes = new Hermes();
        new Foo().onFooCallback(myHermes.pause);

--- js/hermes.js
+++ js/hermes.js
@@ -1,74 +1,76 @@
-
 function Hermes(opts) {
-  this.server         = opts.server;
-  this.namespace      = opts.namespace || '';
-  this.hermesPrepend  = "hermes-msg:"
-  this.subscriptions  = {};
+  var self = this;
 
-  this.resume();
-}
+  this.initialize = function(opts) {
+    self.server         = opts.server;
+    self.namespace      = opts.namespace || '';
+    self.hermesPrepend  = "hermes-msg:"
+    self.subscriptions  = {};
 
-Hermes.prototype = {
+    self.resume();
+  }
 
-  onConnectionOpen: function() {
-    for(var topic in this.subscriptions) {
-      if(!this.subscriptions[topic]) {
-        this.ws.send(topic);
-        this.subscriptions[topic] = true;
+  this.onConnectionOpen = function() {
+      for(var topic in self.subscriptions) {
+      if(!self.subscriptions[topic]) {
+        self.ws.send(topic);
+        self.subscriptions[topic] = true;
       }
     }
-  },
+  }
 
-  onConnectionClose: function() {
+  this.onConnectionClose = function() {
     if(console) {
       console.log("[HERMES] Connection closed.")
     }
 
-    for(var topic in this.subscriptions) {
-      this.subscriptions[topic] = false;
+    for(var topic in self.subscriptions) {
+      self.subscriptions[topic] = false;
     }
-  },
+  }
 
-  onServerMessage: function(e) {
-    if(e.data == '') return;
+  this.onServerMessage = function(e) {
+    if(e.data == '')
+        return;
 
     var msg = JSON.parse(e.data);
     msg.event = e; 
-    HermesEvents.publish(this.hermesPrepend + msg.subscription, [msg])
-  },
+    HermesEvents.publish(self.hermesPrepend + msg.subscription, [msg])
+  }
 
-  subscribe: function(topic, absolute, callback, name) {
+  this.subscribe = function(topic, absolute, callback, name) {
     // absolute is optional, bypasses namespace
-    topic     = (absolute != null && callback != null) ? topic : this.namespace + topic;
+    topic     = (absolute != null && callback != null) ? topic : self.namespace + topic;
     callback  = callback || absolute;
     name      = name || "default";
 
-    if(this.ws.readyState !== 1) {
-      this.subscriptions[topic] = false;
+    if(self.ws.readyState !== 1) {
+      self.subscriptions[topic] = false;
     } else {
-      this.ws.send(topic);
-      this.subscriptions[topic] = true;
+      self.ws.send(topic);
+      self.subscriptions[topic] = true;
     }
 
-    HermesEvents.subscribe(this.hermesPrepend + topic, name, callback);
+    HermesEvents.subscribe(self.hermesPrepend + topic, name, callback);
+  }
+
+  this.pause = function() {
+    self.ws.close(1000);
+    self.ws = null;
+  }
+
+  this.resume = function() {
+    self.ws           = new WebSocket(self.server);
+    self.ws.onmessage = self.onServerMessage.bind(self);
+    self.ws.onopen    = self.onConnectionOpen.bind(self);
+    self.ws.onclose   = self.onConnectionClose.bind(self);
+  }
+
+  this.isActive = function() {
+    return self.ws != null;
   },
 
-  pause: function() {
-    this.ws.close(1000);
-    this.ws = null;
-  },
-
-  resume: function() {
-    this.ws           = new WebSocket(this.server);
-    this.ws.onmessage = this.onServerMessage.bind(this);
-    this.ws.onopen    = this.onConnectionOpen.bind(this);
-    this.ws.onclose   = this.onConnectionClose.bind(this);
-  },
-
-  isActive: function() {
-    return this.ws != null;
-  },
-
+  this.initialize(opts);
 }
 
 // Simple Hermes PUBSUB Helper.
@@ -76,18 +78,18 @@ window.HermesEvents = (function (){
   var cache = {},
 
   publish = function (topic, args, scope) {
-    if(cache[topic]) {
+    if ( cache[topic] ) {
       for(var name in cache[topic]){
-        cache[topic][name].apply(scope || this, args || []);
+        cache[topic][name].apply( scope || this, args || []);
       }
     }
   },
 
-  subscribe = function(topic, name, callback) {
-    if(!cache[topic])
+  subscribe = function (topic, name, callback) {
+    if ( !cache[topic] )
         cache[topic] = {};
 
-    if(cache[topic][name])
+    if ( cache[topic][name] )
       cache[topic][name] = null;
 
     cache[topic][name] = callback;
@@ -101,3 +103,4 @@ window.HermesEvents = (function (){
   };
 
 }());
+
