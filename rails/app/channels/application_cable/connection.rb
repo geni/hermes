0da1956ff822a57af43ad434f7bd43d9b0c6f4a4
Scott Steadman <scott.steadman@geni.com>
2022-08-12 07:39:10 -0700

    finish in ApplicationCable::Connection and tests

--- rails/app/channels/application_cable/connection.rb
+++ rails/app/channels/application_cable/connection.rb
@@ -1,4 +1,56 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :ip, :websocket_key
+
+    def connect
+      self.ip = request.remote_ip
+
+      # Each websocket connection generates a unique key.
+      # This will allow us to differentiate between browser tabs
+      self.websocket_key = request.env['HTTP_SEC_WEBSOCKET_KEY']
+    end
+
+    def encode(cable_message)
+      return if cable_message.has_key?(:type)
+
+      id_hash = JSON.parse(cable_message[:identifier]) rescue {}
+
+      if id_hash['channel'] == HermesChannel.name
+        data = cable_message[:message]
+        data = data[0] == '{' ? JSON.parse(data) : data
+
+        # The hermes client expects this format
+        return {
+          'subscription' => id_hash['topic'],
+          'topic'        => id_hash['topic'],
+          'data'         => data,
+        }.to_json
+      end
+
+      super
+    end
+
+    def decode(websocket_message)
+      if websocket_message[0] == '{'
+        super
+      else
+        # The hermes client just sends the topic name
+        {
+          'command'    => 'subscribe',
+          'identifier' => {
+            'channel'  => HermesChannel.name,
+            'topic'    => websocket_message,
+          }.to_json
+        }
+      end
+    end
+
+    # Disable pings.
+    #
+    # For now we're going to rely on the WebSocket/TCP to notify us
+    # When the WebSocket disconnects (eg via tab closure)
+    def beat
+    end
+
   end
 end
--- rails/test/channels/application_cable/connection_test.rb
+++ rails/test/channels/application_cable/connection_test.rb
@@ -1,11 +1,75 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
-end
+
+  def setup
+    connect '/websocket', :headers => {"REMOTE_ADDR" => '127.0.0.42'}
+
+    # TestCase.connect includes the TestCase::TestConnection module
+    # which overrides the init method, but doesn't set the coder.
+    connection.instance_variable_set(:@coder, ActiveSupport::JSON)
+  end
+
+  test "connect" do
+    assert_equal '127.0.0.42', connection.ip
+  end
+
+  test 'encode returns blank for typed message' do
+    assert_nil connection.encode({:type => 'welcome'})
+  end
+
+  test 'encode returns hermes compatible message' do
+    cable_message = {
+      :identifier => {
+        :channel => HermesChannel.name,
+        :topic   => 'topic',
+      }.to_json,
+      :message    => 'message',
+    }
+
+    expected = {
+      :subscription => 'topic',
+      :topic        => 'topic',
+      :data         => 'message',
+    }.to_json
+    assert_equal expected, connection.encode(cable_message)
+  end
+
+  test 'encode returns cable compatible message' do
+    cable_message = {
+      :identifier => {
+        :channel => 'DummyChannel',
+        :topic   => 'topic',
+      }.to_json,
+      :message    => 'message',
+    }
+
+    expected = cable_message.to_json
+    assert_equal expected, connection.encode(cable_message)
+  end
+
+  test 'decode handles hermes compatible message' do
+    topic = "hermes:test"
+    expected = {
+      "command"    => "subscribe",
+      "identifier" => {
+        "channel"  => "HermesChannel",
+        "topic"    => topic,
+      }.to_json
+    }
+    assert_equal expected, connection.decode(topic)
+  end
+
+  test 'decode handles cable compatible message' do
+    topic = "hermes:test"
+    expected = {
+      "command"    => "subscribe",
+      "identifier" => {
+        "channel"  => "HermesChannel",
+        "topic"    => topic,
+      }.to_json
+    }
+    assert_equal expected, connection.decode(expected.to_json)
+  end
+
+end # class ApplicationCable::ConnectionTest
