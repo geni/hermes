ee05a29ece9cfd6427f969b6fe15e8bfaf37682f
Scott Steadman <scott.steadman@geni.com>
2022-08-17 08:41:07 -0700

    remove secret_key_base override
      This didn't work.

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -39,10 +39,5 @@ module Hermes
     # We're going to do the mounting in config/routes.rb
     config.action_cable.mount_path = nil
 
-    # We're not using this for now
-    def self.secret_key_base
-      'unused'
-    end
-
   end
 end
