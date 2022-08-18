77e793f3d18be034db5280d00c51e0387f05a472
Scott Steadman <scott.steadman@geni.com>
2022-08-18 10:46:25 -0700

    add conifg/deploy/aws_prod

--- rails/config/deploy/aws_prod.rb
+++ rails/config/deploy/aws_prod.rb
@@ -1,85 +1,17 @@
-require 'aws-sdk'
-
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
-set :aws_profile, 'geni-prod'
+set :aws_profile,   'geni-prod'
+set :keep_releases, 2
+set :notify_emails, 'eng@geni.com'
 
 puts "\nRetrieving EC2 host list..."
 
-hosts = get_ec2_hosts(%w[hermes])
-
+hosts = get_ec2_hosts(:app)
 hosts.sort_by!{ |host| host.tags.find{|tag| tag.key == 'Name'}.value }
-
 hosts.each do |host|
-  next unless role_tag = host.tags.find{|tag| tag.key == 'geni:capistrano:roles'}
-
-  name    = host.tags.find {|tag| tag.key == 'Name'}.value
-  ip      = host[:private_ip_address]
-  roles   = role_tag.value.split(/\W+/)
+  name  = host.tags.find {|tag| tag.key == 'Name'}.value
+  ip    = host[:private_ip_address]
+  roles = host.tags.find{|tag| tag.key == 'geni:capistrano:roles'}.value.split(/\W+/)
 
   puts "  Adding: #{name.ljust(20)}#{ip.rjust(13)}, roles: #{roles}"
   server ip, :user => fetch(:local_user), :roles => roles
 end
-
 puts "\n"
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-set :keep_releases, 2
-set :notify_emails, 'eng@geni.com'
-set :server_link,   'https://hermes.geni.com'
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
