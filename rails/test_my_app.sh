81b39b5cfff149f01ba2659c854a7d1a78abd87e
Scott Steadman <scott.steadman@geni.com>
2023-10-24 13:25:10 -0700

    update test_my_app.sh
      I wanted more detail in my test output.

--- rails/test_my_app.sh
+++ rails/test_my_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Usage: time ./test_my_app.sh | tee main.out
+# Usage: time ./test_my_app.sh
 
 # turn on debugging
 #set -x
@@ -8,11 +8,8 @@
 # stop if any command fails
 set -e
 
-# Show which tests are being run and their results
-export TEST_OPTS=--verbose
-
 bundle config set --local clean 'true'
 bundle config set --local path  'vendor/bundle'
 bundle install
-bundle exec rails test
+bundle exec rails test -v
 
