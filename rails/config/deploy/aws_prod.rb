7f9849608b2eac087cc872682f87d2b9b6890478
Scott Steadman <scott.steadman@geni.com>
2023-10-26 13:58:27 -0700

    update aws_prod deployment

--- rails/config/deploy/aws_prod.rb
+++ rails/config/deploy/aws_prod.rb
@@ -2,16 +2,6 @@ set :aws_profile,   'geni-prod'
 set :keep_releases, 2
 set :notify_emails, 'eng@geni.com'
 
-puts "\nRetrieving EC2 host list..."
-
-hosts = get_ec2_hosts(:app)
-hosts.sort_by!{ |host| host.tags.find{|tag| tag.key == 'Name'}.value }
-hosts.each do |host|
-  name  = host.tags.find {|tag| tag.key == 'Name'}.value
-  ip    = host[:private_ip_address]
-  roles = host.tags.find{|tag| tag.key == 'geni:capistrano:roles'}.value.split(/\W+/)
-
-  puts "  Adding: #{name.ljust(20)}#{ip.rjust(13)}, roles: #{roles}"
-  server ip, :user => fetch(:local_user), :roles => roles
-end
-puts "\n"
+# Generated via: cap aws_dev aws:get_server_list
+server '10.21.5.41', :user => 'hermes', :roles => ['app'],  :nickname => 'hermes-010'
+server '10.21.5.46', :user => 'hermes', :roles => ['app'],  :nickname => 'hermes-011'
