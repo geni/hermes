646549fe268b0ca88a82ffdb724b4a621e575d6f
Scott Steadman <scott.steadman@geni.com>
2025-07-10 09:20:39 -0700

    move aws_dev to hermes-020
      I'm moving hermes to a Graviton host to lower costs.

--- rails/config/deploy/aws_dev.rb
+++ rails/config/deploy/aws_dev.rb
@@ -2,4 +2,4 @@ set :keep_releases, 2
 set :notify_emails, 'eng@geni.com'
 
 # Generated via: cap aws_dev aws:get_server_list
-server '10.20.5.51', :user => 'hermes', :roles => ["app"],  :nickname => 'hermes-010'
+server '10.20.5.12', :user => 'hermes', :roles => ["app"],  :nickname => 'hermes-020'
