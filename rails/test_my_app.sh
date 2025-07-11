60e5c0d5da792f0b365f21eaf1adde7902f0a8ee
Scott Steadman <scott.steadman@geni.com>
2025-07-11 07:24:25 -0700

    update test_my_app.sh
      The build is failing. Nokogiri suggests using the force_ruby_platform
      option

--- rails/test_my_app.sh
+++ rails/test_my_app.sh
@@ -8,8 +8,10 @@
 # stop if any command fails
 set -e
 
-bundle config set --local clean 'true'
+bundle config set --local clean true
 bundle config set --local path  'vendor/bundle'
+bundle config set --local force_ruby_platform true
+
 bundle install
 bundle exec rails test -v
 
